89fc07caff3bf399c1a5fb51482b889d7d541324
hrefnick <123642184+hrefnick@users.noreply.github.com>
2023-02-09 10:58:41 -0600

    added documentation

--- Math.php
+++ Math.php
@@ -19,25 +19,32 @@ class Math {
 	 * @param float $b
 	 * @return float
 	 */
+
+	// this is multiplication as well
     public static function mult($num1, $num2){
         $prod = $num1 * $num2;
         return $prod;
     }
+
+	// this is addition
 	public static function add($num1, $num2){
 		$sum = $num1 + $num2;
 		return $sum;
 	}
 
+	// this is subtraction
 	public static function subtract($num1, $num2){
 		$diff = $num1 - $num2;
 		return $diff;
 	}
 
+	// this is multiplication 
     public static function multiplication($num1, $num2){
         $result = $num1 * $num2;
         return $result;
     }
 
+	// this is division
     public static function division($num1, $num2){
         $answer = $num2 / $num1;
         return $answer;
